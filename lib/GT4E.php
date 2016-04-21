303d38687c70a959aeae04b372d7e2a3e1710a5a
Lucas Dominguez <LucasDominguezPro@gmail.com>
2016-04-21 09:43:12 +0200

    Choice of file created added for log

--- lib/GT4E.php
+++ lib/GT4E.php
@@ -1,9 +1,13 @@
 <?php // Goto4ever.com
-function GT4Elog($log) //créé un fichier logs dans le dossier root et y ajoute le contenu en parramètre.
+function GT4Elog($log, $file = "logs.log") //créé un fichier logs dans le dossier root et y ajoute le contenu en parramètre.
 {
-  $file = "logs.log";
   $fileopen=(fopen("$file",'a'));
   fwrite($fileopen, $today = date("Y-m-d H:i:s")." -> ".$log."\r\n"); // date-heure + log
   fclose($fileopen);
+}
+
+function GT4Esitemap()
+{
+
 }
 ?>
