8d7bb778e177d30f58175851a11afeef138e2424
Lucas Dominguez <LucasDominguezPro@gmail.com>
2016-04-21 09:52:29 +0200

    Indented with 4 spaces

--- lib/GT4E.php
+++ lib/GT4E.php
@@ -1,13 +1,13 @@
 <?php // Goto4ever.com
 function GT4Elog($log, $file = "logs.log") //créé un fichier logs dans le dossier root et y ajoute le contenu en parramètre.
 {
-  $fileopen=(fopen("$file",'a'));
-  fwrite($fileopen, $today = date("Y-m-d H:i:s")." -> ".$log."\r\n"); // date-heure + log
-  fclose($fileopen);
+    $fileopen=(fopen("$file",'a'));
+    fwrite($fileopen, $today = date("Y-m-d H:i:s")." -> ".$log."\r\n"); // date-heure + log
+    fclose($fileopen);
 }
 
 function GT4Esitemap()
 {
-
+    
 }
 ?>
