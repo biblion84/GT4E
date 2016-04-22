1723046067eb3241872b237311ca3c4ebf748549
blipn <oblipn@gmail.com>
2016-04-22 10:27:32 +0200

    GT4Elog() can now be used without argument

--- lib/GT4E.php
+++ lib/GT4E.php
@@ -1,9 +1,28 @@
 <?php // Goto4ever.com
-function GT4Elog($log, $file = "logs.log") //créé un fichier logs dans le dossier root et y ajoute le contenu en parramètre.
+function GT4Elog($log = NULL, $file = "logs.log") //Add a logfile in root directory and write logs
 {
+    if ($log == NULL) {
+        $log = generateCallTrace();
+    }
     $fileopen=(fopen("$file",'a'));
-    fwrite($fileopen, $today = date("Y-m-d H:i:s")." -> ".$log."\r\n"); // date-heure + log
+    fwrite($fileopen, $today = date("Y-m-d H:i:s")." -> ".$log."\r\n"); // DATETIME + LOG
     fclose($fileopen);
+}
+
+function generateCallTrace() // From jurchiks101 at gmail dot com = Return something like that : 1) X:\www\backend\index.php(15): include('X:\\Tools\\wamp\\w...')
+{
+    $e = new Exception();
+    $trace = explode("\n", $e->getTraceAsString());
+    $trace = array_reverse($trace); // reverse array to make steps line up chronologically
+    array_shift($trace); // remove {main}
+    array_pop($trace); // remove call to this method
+    $length = count($trace);
+    $result = array();
+    for ($i = 0; $i < $length-1; $i++)
+    {
+        $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
+    }
+    return "\t" . implode("\n\t", $result);
 }
 
 function GT4Esitemap()
